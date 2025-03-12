a1223bbe1ff20f65a8a09344bfffc1a4634bd107
ImperatorYarik! <yariktb124@gmail.com>
2025-03-12 23:25:03 +0200

    Implemented docker configuration

--- api/routes/api.php
+++ api/routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::prefix('api')->group(function () {
+    Route::get('/', function () {
+        return response()->json([
+            'message' => 'Hello from Laravel API'
+        ]);
+    });
+});
